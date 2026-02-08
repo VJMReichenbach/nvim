caec9c3f32ad646b9c2035b9d4a0a9f59a6d8b23
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-02-08 17:59:38 +0100

    Updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,17 @@
 return { -- Fuzzy Finder (files, lsp, etc)
   'nvim-telescope/telescope.nvim',
-  event = 'VimEnter',
-  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    {
       'nvim-telescope/telescope-fzf-native.nvim',
 
       build = 'make',
-
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
     },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
-    require('telescope').setup {
-      defaults = {
-        wrap_results = true,
-      },
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
-        },
-      },
-    }
-
     -- enable wrapping in preview window
     vim.api.nvim_create_autocmd('User', {
       pattern = 'TelescopePreviewerLoaded',
@@ -38,10 +20,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
         vim.wo.number = true
       end,
     })
-
-    -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
@@ -63,16 +41,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
       builtin.help_tags,
       { desc = '[F]ind in [H]elp' }
     )
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>fc', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(
-        require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        }
-      )
-    end, { desc = '[F]uzzily search in [C]urrent buffer' })
+    vim.keymap.set(
+      'n',
+      '<leader>fc',
+      builtin.current_buffer_fuzzy_find,
+      { desc = '[F]uzzily search in [C]urrent buffer' }
+    )
   end,
 }
