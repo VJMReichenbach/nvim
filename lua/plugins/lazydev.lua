3ba837c1a3688ad7a092d80dac5df4a572e2dad3
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-02-08 18:35:23 +0100

    removed opts from lazydev config

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,10 +4,5 @@
 return {
   'folke/lazydev.nvim',
   ft = 'lua',
-  opts = {
-    library = {
-      -- Load luvit types when the `vim.uv` word is found
-      { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-    },
-  },
+  opts = {},
 }
