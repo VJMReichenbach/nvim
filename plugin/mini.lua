d612f651827099375a3ac2f69440b7f9dbcf2a27
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-03-26 15:23:43 +0100

    made mini pick window centered

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -31,7 +31,22 @@ statusline.section_location = function()
 end
 
 -- Telescope replacement
-require('mini.pick').setup()
+local win_config = function()
+  local height = math.floor(0.618 * vim.o.lines)
+  local width = math.floor(0.618 * vim.o.columns)
+  return {
+    anchor = 'NW',
+    height = height,
+    width = width,
+    row = math.floor(0.5 * (vim.o.lines - height)),
+    col = math.floor(0.5 * (vim.o.columns - width)),
+  }
+end
+require('mini.pick').setup {
+  window = {
+    config = win_config,
+  },
+}
 
 vim.keymap.set(
   'n',
