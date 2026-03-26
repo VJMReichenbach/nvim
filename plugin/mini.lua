7b38298ddab1bf3fa7d89a0494eacc615e3cc017
VJMReichenbach <Valentin.Reichenbach@live.com>
2026-03-26 15:41:28 +0100

    used mini.move

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -66,3 +66,19 @@ vim.keymap.set(
   MiniPick.builtin.help,
   { desc = '[F]ind in [H]elp' }
 )
+
+require('mini.move').setup {
+  mappings = {
+    -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+    left = '<M-H>',
+    right = '<M-L>',
+    down = '<M-J>',
+    up = '<M-K>',
+
+    -- Move current line in Normal mode
+    line_left = '<M-H>',
+    line_right = '<M-L>',
+    line_down = '<M-J>',
+    line_up = '<M-K>',
+  },
+}
